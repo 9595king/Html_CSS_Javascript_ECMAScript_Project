0a1e09f0b05d7077cc52bd544c902c764be9509b
9595king <9595king@gmail.com>
2026-09-17 14:15:16 +0900

    2026.09.17 Updates

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,9 +1,11 @@
 import AppTodo from './AppTodo';
+//import UseCallbackApp from './components/UseCallbackApp';
 
 const App = () => {
     return (
         <div>
             <AppTodo />
+            {/* <UseCallbackApp /> */}
         </div>
     );
 };
